667d2ac1b930b649b020ebc2c09252cc90ed22d5
jayming66 <mjw8941@gmail.com>
2023-12-26 16:24:04 +0900

    feat : main page css 완성

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import styled from "styled-components";
 
 const BodyContainer = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -10,17 +11,82 @@ const BodyContainer = styled.div`
   background: #032746;
   width: 100%;
   height: 100vh;
-  color: white;
-  font-family: var(--font-googleInterRegular);
-  font-size: 60px ;
+  
 `;
 
 const MainContainer = styled.div`
-  width: 100%;
+  position: relative;
 `;
 
-const Maintitle = styled.h1`
+const LandingTextContainer = styled.div`
+  position: absolute;
+  left: 8%;
+  top: 15%;
+  width: 100%;
+  height: 100%;
+`;
 
+const Maintitle = styled.div`
+  position: absolute;
+  color: white;
+  font-family: var(--font-googleInterRegular);
+  font-size: 60px;
+`;
+
+const Subtitle = styled.div`
+  position: absolute;
+  top:13%;
+  color: white;
+  font-family: var(--font-googleInterRegular);
+  font-size: 30px;
+`;
+
+const LandingButtonElseContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 8%;
+  color: white;
+  font-family: var(--font-googleInterRegular);
+  font-size: 60px;
+`;
+
+const LandingButton = styled.button`
+  color: black;
+  font-family: var(--font-googleInterRegular);
+  font-size: 20px;
+  background: white;
+  padding: 20px 50px;
+  border-radius: 10px;
+  text-align: center;
+  margin-right: 50px;
+`;
+
+const SecondButton = styled(LandingButton)`
+  background: #032746;
+  color: white;
+  border: 2px solid white;
+`;
+
+const LandingImage = styled.img`
+  position: absolute;
+  width: 600px;
+  top : 30%;  
+  left : 45%;
+`;
+
+const LandingButtonGuide = styled.div`
+  color: white;
+  font-family: var(--font-googleInterRegular);
+  font-size: 15px;
+  position: absolute;
+  left: 8%;
+  top: 47%;
+`;
+
+const BoldSpan = styled.span`
+  font-weight: bold;
+  font-size: 17px;
+  
 `;
 
 
@@ -29,9 +95,19 @@ export default function Main() {
     <MainContainer>
       <Header />
       <BodyContainer>
-        <Maintitle></Maintitle>
-        <h1>The First Web3 Affiliate Marketing</h1>
-        
+        <LandingTextContainer>
+          <Maintitle>The First Web3 Affiliate Marketing</Maintitle>
+          <Subtitle>Generate Link, Share to Others, and Earn Tokens</Subtitle>
+        </LandingTextContainer>
+        <LandingButtonGuide>
+          To reach services Split has to offer, you'll need to{" "}
+          <BoldSpan>Connect Wallet</BoldSpan>.
+        </LandingButtonGuide>
+        <LandingButtonElseContainer>
+          <LandingButton>Get Started {"\b >"} </LandingButton>
+          <SecondButton>Documantation {"\b >"} </SecondButton>
+        </LandingButtonElseContainer>
+        <LandingImage src="langingpage-image1.png" />
       </BodyContainer>
     </MainContainer>
   );
